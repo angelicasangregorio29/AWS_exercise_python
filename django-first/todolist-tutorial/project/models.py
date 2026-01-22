4b475be951da6d3f3d83e52f25c861d73ff5339f
angel <angelica29sangregorio@gmail.com>
2026-01-22 22:42:29 +0100

    Django Todo List Backend - Walkthrough

--- django-first/todolist-tutorial/project/models.py
+++ django-first/todolist-tutorial/project/models.py
@@ -4,4 +4,13 @@ from django.db import models
 
 class Project(models.Model):
     title = models.CharField(max_length=200)
-    is_available = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
+
+class Task(models.Model):
+    description = models.CharField(max_length=200)
+    project = models.ForeignKey(Project, on_delete=models.CASCADE, related_name='tasks')
+
+    def __str__(self):
+        return self.description
