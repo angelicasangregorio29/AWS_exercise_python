65c0e0836355d6584b44e6dbf13c1124037e1f11
angel <angelica29sangregorio@gmail.com>
2026-01-28 10:14:26 +0100

    create django-todolist

---
+++ django-todolist/activate.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+echo "======================="
+echo "start activaction venv"
+echo "======================="
+
+source venv/bin/activate
+
+echo "======================="
+echo "venv in esecuzione"
+echo "======================="
+
+cd todolist
+
+echo "======================="
+echo "ti trovi in:"
+pwd
+echo "======================="
+
+python manage.py runserver
